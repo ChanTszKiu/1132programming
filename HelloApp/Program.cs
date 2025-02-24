1cabd8961269a31bce475f6183a3d6b7a11cf9aa
ChanTszKiu <s1102516@gm.pu.edu.tw>
2025-02-24 10:25:41 +0800

    加入專案檔案。

---
+++ HelloApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, C#!");
+            Console.WriteLine("How do you do?");
+            Console.WriteLine("Let's do it.");
+
+            Console.ReadKey();
+        }
+    }
+}
