bb9962ebe3869b18b26dfea210843681c3383fd7
ChanTszKiu <s1102516@gm.pu.edu.tw>
2025-03-10 11:29:26 +0800

    HW4

---
+++ HW4-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            if (input.Length != 2 || !int.TryParse(input[0], out int x) || !int.TryParse(input[1], out int y) || x <= 0 || y <= 0)
+            {
+                Console.WriteLine("請輸入有效的正整數。");
+                return;
+            }
+
+            int min = Math.Min(x, y);
+            int max = Math.Max(x, y);
+            int sum = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
