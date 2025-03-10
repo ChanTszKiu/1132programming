9a4283b807baad9c09f0934d3bc6e84df2acaab7
ChanTszKiu <s1102516@gm.pu.edu.tw>
2025-03-10 10:54:21 +0800

    forloop

---
+++ ForLoopApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ForLoopApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0;i < 10; i++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+            //Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
