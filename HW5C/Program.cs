2d1c51277bf18f32e476e0c8c7226915b7ad0200
ChanTszKiu <s1102516@gm.pu.edu.tw>
2025-03-17 09:26:59 +0800

    5C

---
+++ HW5C/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW5C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 讀取輸入
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+
+            // 計算判別式 D = b² - 4ac
+            int D = b * b - 4 * a * c;
+
+            // 判斷根的種類
+            if (D > 0)
+                Console.WriteLine("Two different roots");
+            else if (D == 0)
+                Console.WriteLine("Two same roots");
+            else
+                Console.WriteLine("No real root");
+
+            Console.ReadKey();
+        }
+    }
+}
