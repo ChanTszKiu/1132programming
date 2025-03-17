ab41316408fb2d02ebb5a84a2043fd82a50ab402
ChanTszKiu <s1102516@gm.pu.edu.tw>
2025-03-17 09:24:49 +0800

    5B

---
+++ HW5B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW5B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 讀取點的座標
+            string[] input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+
+            // 計算距離 d(O, A)
+            double distance = Math.Sqrt(x * x + y * y);
+
+            // 輸出格式
+            Console.WriteLine($"O(0, 0)");
+            Console.WriteLine($"A({x}, {y})");
+            Console.WriteLine($"d(O, A) = {distance:F2}");
+
+            // 判斷是否在圓內
+            if (distance <= 100)
+                Console.WriteLine("inside");
+            else
+                Console.WriteLine("outside");
+
+            Console.ReadKey();
+        }
+    }
+}
